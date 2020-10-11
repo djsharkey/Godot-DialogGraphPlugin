c055179c9b7fe5780c3744a0d296986169b27eab
Dylan Sharkey <serghar12@gmail.com>
2020-10-11 14:36:23 -0700

    remove translations, to be reimplemented later

--- src/SpeechGraphNode.gd
+++ src/SpeechGraphNode.gd
@@ -2,5 +2,4 @@ tool
 extends "res://addons/Godot-DialogGraphPlugin/src/GraphNode.gd"
 
 func set_node_line(node_line):
-	node_line.set_first_label("Speech Code " + String(node_lines.size()) + ":")
-	node_line.set_second_label("Translation " + String(node_lines.size()) + ":")
+	node_line.set_label("Speech Code " + String(node_lines.size()) + ":")
